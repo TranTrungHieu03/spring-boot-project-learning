08579847c9a75bde8767afd9cf3eae14e9f55d9e
hieutt03 <hieuttjob@gmail.com>
2024-07-21 00:18:57 +0700

    init project spring boot

---
+++ src/main/java/com/hieutt03/springboot/mydemowebapp/MyDemoWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.hieutt03.springboot.mydemowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemoWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDemoWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hieutt03/springboot/mydemowebapp/MyDemoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieutt03.springboot.mydemowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
