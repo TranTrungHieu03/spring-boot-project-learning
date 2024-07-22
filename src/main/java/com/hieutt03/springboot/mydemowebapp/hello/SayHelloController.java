57720a8a31b9ef3dc7afb377806c6624e04fb0da
hieutt03 <hieuttjob@gmail.com>
2024-07-22 23:17:30 +0700

    jsp view

--- src/main/java/com/hieutt03/springboot/mydemowebapp/hello/SayHelloController.java
+++ src/main/java/com/hieutt03/springboot/mydemowebapp/hello/SayHelloController.java
@@ -4,11 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.nio.Buffer;
+
 @Controller
 public class SayHelloController {
     @RequestMapping("say-hello")
     @ResponseBody
     public String sayHello(){
          return "Hello! What are you doing?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My HTML Page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h1>Hello World</h1>");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+
+    //"say-hello-jsp" => sayHello.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jps/welcome.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jps/login.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jps/todos.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,4 @@
+<html>
+<title>My HTML Page</title>
+<body>My HTML JSP Page with body - Change</body>
+</html>
